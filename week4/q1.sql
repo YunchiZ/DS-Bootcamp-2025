a1b50126731cba246122fdf869cf7ccb825339cc
YunchiZ <scyyz17@gmail.com>
2025-04-02 14:29:34 -0400

    correct structure

--- week4/q1.sql
+++
@@ -1,7 +0,0 @@
-SELECT COUNT(*) AS total_orders
-FROM orders o
-JOIN customers c ON o.customer_id = c.customer_id
-WHERE o.order_date = '2023-03-18'
-  AND c.first_name = 'John'
-  AND c.last_name = 'Doe'
-  AND o.status = 'completed';
